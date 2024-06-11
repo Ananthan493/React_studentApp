6385cd5db881b35fdd95961e9a11455eeb7376b2
Ananthan493 <ananthanofficial493@gmail.com>
2024-06-11 14:20:08 +0530

    api integrated

--- src/components/AddStudent.jsx
+++ src/components/AddStudent.jsx
@@ -1,7 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import NavBar from './NavBar'
+import axios from 'axios'
 
 const AddStudent = () => {
+    const[data,setData]=useState(
+        {
+            "Name": "",
+            "Rollno": "",
+            "Admno": "",
+            "College": ""
+        }
+    )
+    const inputHandler = (event) => {
+        setData({...data, [event.target.name]:event.target.value})
+      }
+      const readValue = () => {
+            console.log(data)
+            axios.post("http://localhost:8080/add",data).then(
+            (response)  => {
+              console.log(response.data)
+              if(response.data.status=="success")
+                {
+                  alert("successfully added")
+                }
+                else{
+                  alert("error")
+                }
+            }).catch(
+                (error)=>{
+                  console.log(error.message)
+                  alert(error.message)
+                }
+              )
+          }
     return (
         <div>
             <NavBar/>
@@ -11,42 +42,23 @@ const AddStudent = () => {
                         <div className="row g-3">
                             <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                                 <label htmlFor="" className="label form-label">Name</label>
-                                <input type="text" className="form-control" />
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                                <label htmlFor="" className="label form-label">Admin.no.</label>
-                                <input type="text" className="input form-control" />
+                                <input type="text" className="form-control" name='Name'value={data.Name} onChange={inputHandler} />
                             </div>
                             <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                                 <label htmlFor="" className="label form-label">Roll No.</label>
-                                <input type="text" className="input form-control" />
+                                <input type="text" className="input form-control" name='Rollno' value={data.Rollno} onChange={inputHandler} />
                             </div>
                             <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                                <label htmlFor="" className="label form-label">Parent Name</label>
-                                <input type="text" className="input form-control" />
+                                <label htmlFor="" className="label form-label">Admn No.</label>
+                                <input type="text" className="input form-control" name='Admno' value={data.Admno} onChange={inputHandler}/>
                             </div>
-                            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                                 <label htmlFor="" className="label form-label">College</label>
-                                <textarea name="" id="" className="form-control"></textarea>
+                                <input type="text" className="input form-control" name='College' value={data.College} onChange={inputHandler} />
                             </div>
                             <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                                <label htmlFor="" className="label form-label">DOB</label>
-                                <input type="date" name="" id="" className="form-control" />
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                                <label htmlFor="" className="label form-label">Email</label>
-                                <input type="text" className="input form-control" />
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                                <label htmlFor="" className="label form-label">Password</label>
-                                <input type="text" className="input form-control" />
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                                <label htmlFor="" className="label form-label">Confirm Password</label>
-                                <input type="text" className="input form-control" />
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                                <button className="btn btn-success">Submit</button>
+                                <button className="btn btn-success" onClick={readValue}>Submit</button>
                             </div>
                         </div>
                     </div>
--- src/components/ViewAll.jsx
+++ src/components/ViewAll.jsx
@@ -1,100 +1,51 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import NavBar from './NavBar'
+import axios from 'axios'
 
 const ViewAll = () => {
+    const[data,changeData]=useState([])
+    const fetchData = () => {
+        axios.get("http://localhost:8080/view").then(
+            (response) => {
+                changeData(response.data)
+            }
+        ).catch().finally()
+    }
+    useEffect(() => { fetchData() }, [])
     return (
         <div>
             <NavBar/>
             <div className="container">
-                <div className="row">
-                    <div className="col col-12 col-sm-12 col-lg-12 col-xl-12 col-xxl-12">
-                        <div className="row g-3">
-                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-                                <div class="card">
-                                    <img src="https://upload.wikimedia.org/wikipedia/en/1/19/Bruce_Wayne_%28The_Dark_Knight_Trilogy%29.jpg" class="card-img-top" alt="..." height="350px"/>
-                                        <div class="card-body">
-                                            <p class="card-text">Bruce Wayne </p>
-                                        </div>
-                                </div>
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-                            <div class="card">
-                                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSzMOV1VsJ98I8HwSFMteZh9sdZtLe-3VJsPQ&s" class="card-img-top" alt="..." height="350px"/>
-                                        <div class="card-body">
-                                            <p class="card-text">Steve Rogers </p>
-                                        </div>
-                                </div>
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-                            <div class="card">
-                                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Robert_Downey%2C_Jr._2012.jpg/1200px-Robert_Downey%2C_Jr._2012.jpg" class="card-img-top" alt="..." height="350px"/>
-                                        <div class="card-body">
-                                            <p class="card-text">Tony Starck</p>
-                                        </div>
-                                </div>
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-                            <div class="card">
-                                    <img src="https://m.media-amazon.com/images/M/MV5BNzgxYTA2OTUtYmE0ZC00ZTc0LWJjY2QtOTIzMTJhNGUyZjBlXkEyXkFqcGdeQXVyMTg4NDI0NDM@._V1_.jpg" class="card-img-top" alt="..." height="350px"/>
-                                        <div class="card-body">
-                                            <p class="card-text">Diana(wonder Women)</p>
-                                        </div>
-                                </div>
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-                            <div class="card">
-                                    <img src="https://static.miraheze.org/greatcharacterswiki/thumb/2/2b/DominicToretto.jpg/300px-DominicToretto.jpg" class="card-img-top" alt="..." height="350x"/>
-                                        <div class="card-body">
-                                            <p class="card-text">Dominic Toretto </p>
-                                        </div>
-                                </div>
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-                            <div class="card">
-                                    <img src="https://ew.com/thmb/IWTSy6GrKCmZBSIcZwC84oHmNdw=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/mos-10175c_2-2000-116d947875b84ddcabb01baa1fa1b389.jpg" class="card-img-top" alt="..." height="350x"/>
-                                        <div class="card-body">
-                                            <p class="card-text">Clark Kent </p>
-                                        </div>
-                                </div>
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-                            <div class="card">
-                                    <img src="https://staticg.sportskeeda.com/editor/2023/11/e56bb-16999499120522.png" class="card-img-top" alt="..." height="350px"/>
-                                        <div class="card-body">
-                                            <p class="card-text">Letty</p>
-                                        </div>
-                                </div>
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-                            <div class="card">
-                                    <img src="https://upload.wikimedia.org/wikipedia/en/7/78/Furious_7_Brian_O%27Conner.jpg" class="card-img-top" alt="..." height="350px"/>
-                                        <div class="card-body">
-                                            <p class="card-text">Brian</p>
-                                        </div>
-                                </div>
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-                            <div class="card">
-                                    <img src="https://www.hollywoodreporter.com/wp-content/uploads/2018/04/infinitywar5a4bb0cd79630-h_2018.jpg?w=1296" class="card-img-top" alt="..." height="350px"/>
-                                        <div class="card-body">
-                                            <p class="card-text">Thanos</p>
-                                        </div>
-                                </div>
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-                            <div class="card">
-                                    <img src="https://static.toiimg.com/photo/84582749/84582749.jpg?v=3" class="card-img-top" alt="..." height="350px"/>
-                                        <div class="card-body">
-                                            <p class="card-text">Natasha</p>
-                                        </div>
-                                </div>
-                            </div>
-                        </div>
+                <div className="row g-3">
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th scope="col">Name</th>
+                                    <th scope="col">Roll No</th>
+                                    <th scope="col">Admn No.</th>
+                                    <th scope="col">College</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {
+                                    data.map(
+                                        (value, index) => {
+                                            return <tr>
+                                                <td>{value.Name}</td>
+                                                <td>{value.Rollno}</td>
+                                                <td>{value.Admno}</td>
+                                                <td>{value.College}</td>
+                                            </tr>
+                                        }
+                                    )
+                                }
+                            </tbody>
+                        </table>
                     </div>
                 </div>
             </div>
         </div>
     )
 }
-
 export default ViewAll
